552b2a6ae4053cfb405c731cc4d7d3148540f691
Anjana Yadav <anjana.yadav19@gmail.com>
2023-05-24 00:00:11 +0530

    creating conflict in A

--- src/a1/StringExample1.java
+++ src/a1/StringExample1.java
@@ -2,6 +2,7 @@ package a1;
 
 public class StringExample1 {
   public static void main(String[] args) {   
-	System.out.println("Branch master");
+	System.out.println("con flict in Branch master");
+	System.out.println("Branch A1);
 }
 }
