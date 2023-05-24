8446f8fb09b8c9a9c2a1e8174b2503c516edc93c
Anjana Yadav <anjana.yadav19@gmail.com>
2023-05-24 15:54:02 +0530

    Conflict resolved in b3

--- src/a1/StringExample1.java
+++ src/a1/StringExample1.java
@@ -4,5 +4,8 @@ public class StringExample1 {
   public static void main(String[] args) {   
 	System.out.println("Creating C3- A1conflict in Branch master");
 	System.out.println("Branch A3");
+	System.out.println("Change C3 B1 conflict in Branch master");
+	System.out.println("Branch B3");
   }
+
 }
