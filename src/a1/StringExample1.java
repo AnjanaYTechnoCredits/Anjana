b503d8fbd0e8959b40bcd1744294211b38855fae
Anjana Yadav <anjana.yadav19@gmail.com>
2023-05-26 15:55:44 +0530

    creating conflict in A1

--- src/a1/StringExample1.java
+++ src/a1/StringExample1.java
@@ -2,9 +2,9 @@ package a1;
 
 public class StringExample1 {
   public static void main(String[] args) {   
-	System.out.println("Creating C3- A1conflict in Branch master");
-	System.out.println("Branch A3");
-	System.out.println("Change C3 B1 conflict in Branch master");
+	System.out.println("Creating A1- A1conflict in Branch master");
+	System.out.println("Branch A1");
+	System.out.println("Change A1 conflict in Branch master");
 	System.out.println("Branch B3");
   }
 
